73f145af8a12b51d54eb4f549a922699196231f8
kyle macpherson <kaimac2010@gmail.com>
2015-10-23 11:13:06 +0100

    completed tasks hidden

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-(function() {
-var toDoList = angular.module('ToDoList', ['ngResource']);
+(function () {
+  var toDoList = angular.module('ToDoList', ['ngResource']);
 
-toDoList.controller('ToDoController', function(){
-  this.list = tasks;
-});
+  toDoList.controller('ToDoController', function (){
+    this.list = tasks;
+  });
 
-var tasks = {
-  name: 'Make Todo list'
-  }
+  var tasks = [
+    {
+      name: 'Make Todo list',
+      completed: false
+    }
+  ];
+
 })();
