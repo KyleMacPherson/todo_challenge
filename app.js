e4755869c86c32ab3efbd1be1f33072cf922e0fb
kyle macpherson <kaimac2010@gmail.com>
2015-10-24 19:49:33 +0100

    show active, show completed, show all filters working

--- app.js
+++ app.js
@@ -5,13 +5,42 @@
     var self = this;
     this.list = [];
     this.addTask = function (task) {
-      self.list.push({name: task, completed: false})
+      self.list.push({name: task, completed: false, showing: true})
     }
+
     this.deleteTask = function (index) {
       self.list.splice(index, 1);
     }
+
     this.markTaskCompleted = function (index) {
       self.list[index].completed = true
     };
+
+    this.showCompleted =  function () {
+      for (var i = 0; i < self.list.length; i++) {
+        if(self.list[i].completed){
+          self.list[i].showing = true
+        } else {
+          self.list[i].showing = false
+        }
+      }
+    };
+
+    this.showActive =  function () {
+      for (var i = 0; i < self.list.length; i++) {
+        if(self.list[i].completed){
+          self.list[i].showing = false
+        } else {
+          self.list[i].showing = true
+        }
+      }
+    };
+
+    this.showAll =  function () {
+      for (var i = 0; i < self.list.length; i++) {
+          self.list[i].showing = true
+      }
+    };
+
   });
 })();
